29e062a1676625590ca051405c598e9aaf0836c4
Andre Pedchenko <pan.sf@yandex.ru>
2020-10-25 15:20:27 +0400

    make it better

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/index')
+@app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>hmrs folder</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
