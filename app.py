8f5dad9298b8407eed8e282f8bd8096b6b1a4d12
Andre Pedchenko <pan.sf@yandex.ru>
2020-10-25 18:57:26 +0400

    upd

--- app.py
+++ app.py
@@ -18,19 +18,47 @@ def marusya():
 def main():
 
     card = {}
-    buttons = {}
+    buttons = []
+    end_session = False
+    text = ''
 
     if request.json['session']['new']:
         text = 'Это еще тестовый навык'
-
-
-
+    elif request.json['request']['command'].lower() == 'привет':
+        text = 'Привет!!!'
+    elif request.json['request']['command'].lower() == 'хватит':
+        text = 'Хорошо, до встречи!'
+        end_session = True
+    elif request.json['request']['command'].lower() == 'картинка':
+        text = 'Вот такая есть картинка:'
+        card = {
+            'type': 'BigImage',
+            'image_id': 457239018,
+            'title': 'Это Коржик — офисный пёс',
+            'description': ''
+                }
+    elif request.json['request']['command'].lower() == 'кнопки':
+        text = 'Вот такие есть кнопки:'
+        buttons = [
+                    {'title': 'кнопка 1'},
+                    {'title': 'кнопка 2'}
+                  ]
+    elif request.json['request']['command'].lower() == 'карусель':
+        text = 'Карусель:'
+        card = {
+            "type": "ItemsList",
+            "title": "Две картинки",
+            "description": "",
+            "items": [{"image_id": 457239018}, {"image_id": 457239017}]
+                }
+    else:
+        text = request.json['request']['command'].lower()
 
     response = {
     'version': request.json['version'],
     'session': request.json['session'],
     'response': {
-        'end_session': False,
+        'end_session': end_session,
         'text': text,
         'card': card,
         'buttons': buttons
